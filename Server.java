42bd78b5e399a2d1b5dc19c08574c53f50be426a
b@8D5ft0 <akshayk0406@gmail.com>
2016-04-10 15:54:32 -0500

    Added Server functionality

---
+++ Server.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+import java.net.InetAddress;
+import org.apache.thrift.transport.*;
+import org.apache.thrift.TException;
+import java.net.UnknownHostException;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+
+public class Server
+{
+	private static String CONFIG_FILE_NAME				= "";
+	private static String CURRENT_NODE_IP				= "";
+	private static String SERVER_PORT_KEY				= "ServerPort";
+	private static String SERVER_IP_KEY					= "ServerIP";
+	private static String INPUT_DIRECTORY_KEY			= "InputDirectory";
+	private static String INTERMEDIATE_DIRECTORY_KEY	= "IntermediateDirectory";
+	private static String OUTPUT_DIRECTORY_KEY			= "OutputDirectory";
+	private static String CHUNK_SIZE_KEY				= "ChunkSize";
+	private static String MERGE_SIZE_KEY				= "MergeTaskSize";
+	
+	public static void main(String targs[]) throws TException
+	{
+		try
+		{
+			CURRENT_NODE_IP			= InetAddress.getLocalHost().getHostName();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Unable to get hostname ....");
+		}
+
+		if(CURRENT_NODE_IP=="")
+		{
+			System.out.println("Unable to get Current System's IP");
+			return;
+		}
+		if(targs.length==1)
+		{
+			CONFIG_FILE_NAME					= targs[0];
+		}
+		else
+		{
+			System.out.println("Config file missing!!!");
+			return;
+		}
+
+		HashMap<String,String> configParam	= Util.getInstance().getParameters(CONFIG_FILE_NAME);
+		Node currentNode					= new Node(Integer.parseInt(configParam.get(SERVER_IP_KEY)),Integer.parseInt(configParam.get(SERVER_PORT_KEY)));
+		SortServiceHandler sortService		= new SortServiceHandler(currentNode,configParam.get(INPUT_DIRECTORY_KEY),configParam.get(INTERMEDIATE_DIRECTORY_KEY),
+																	configParam.get(OUTPUT_DIRECTORY_KEY),Integer.parseInt(configParam.get(CHUNK_SIZE_KEY)),
+																	Integer.parseInt(configParam.get(MERGE_SIZE_KEY)));
+		TThreadPoolServer server			= Util.getInstance().getQuorumServer(Integer.parseInt(configParam.get(SERVER_PORT_KEY)),sortService);
+		server.serve();	
+	}
+}
---
+++ Util.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+import org.apache.thrift.transport.*;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.protocol.TBinaryProtocol;
+
+public class Util
+{
+	private static int MOD 		= 107;
+	private static Util util 	= null;
+
+	//Creating Singleton instance of the class
+	public static Util getInstance()
+	{
+		if(util==null)
+			util	= new Util();
+		return util;
+	}
+	
+	public static TThreadPoolServer getServer(int Port,SortServiceHandler sortService) throws TTransportException
+	{
+		TServerTransport serverTransport    = new TServerSocket(Port);
+        TTransportFactory factory           = new TFramedTransport.Factory();
+        SortService.Processor processor   	= new SortService.Processor(sortService);
+        TThreadPoolServer.Args args         = new TThreadPoolServer.Args(serverTransport);
+        args.processor(processor);
+        args.transportFactory(factory);
+		return new TThreadPoolServer(args);
+	}
+
+	public static HashMap<String,String> getParameters(String filename)
+	{
+		BufferedReader br	= null;
+		String content		= "";
+		HashMap<String,String> params	= new HashMap<String,String>();
+		try
+		{
+			br				= new BufferedReader(new FileReader(filename));
+			while((content = br.readLine())!=null)
+			{
+				String [] tokens 	= content.split(":");
+				params.put(tokens[0],tokens[1]);
+			}
+		}
+		catch(IOException e) {}
+		finally
+		{
+			try
+			{
+				if(br!=null) br.close();
+			}
+			catch(IOException e){}
+		}
+		return params;
+	}
+}
