560800cadef795d055e0c6d2ae3bccf0d6cfd3a1
Rohan Sadale <sadal005@trombone.cs.umn.edu>
2016-04-11 21:32:16 -0500

    Adding doMerge function

--- ComputeServiceHandler.java
+++ ComputeServiceHandler.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 import java.lang.System;
 import java.lang.Runnable;
 import java.util.concurrent.*;
@@ -38,8 +39,8 @@ public class ComputeServiceHandler implements Compute.Iface{
 
 		// Write output to file		
 		FileWriter fw = new FileWriter(INTERMEDIATE_DIRECTORY_KEY + fileName);
-		for(int i = 0 ; i < items.length(); i++)
-			fw.write(items[i] + "\n");
+		for(int i = 0 ; i < items.size(); i++)
+			fw.write(String.valueOf(items[i]) + "\n");
 		
 		long stopTime = System.currentTimeMillis();
 		long elapsedTime = stopTime - startTime;
@@ -48,9 +49,73 @@ public class ComputeServiceHandler implements Compute.Iface{
 	} 
 	
 	@Override
-	public Time doMerge(List<String> files){
+	// Add two more parameters
+	//	1) Chunk no
+	//	2) Intermediate Merge or Final Merge
+	public static Time doMerge(List<String> files){
+
+		long startTime = System.currentTimeMillis();	
+		int n = files.size();
+		int [] numbers = new int[n];
+		BufferedReader[] fp = new BufferedReader[n];
 		
+		try{		
+		for(int i = 0; i < n; i++){
+//			String fileName = files[i];
+			fp[i] = new BufferedReader(new FileReader(INTERMEDIATE_DIRECTORY_KEY + files.get(i)));
+			String no = fp[i].readLine();
+			if(no != null)
+				numbers[i] = Integer.parseInt(no);
+			else
+				numbers[i] = Integer.MAX_VALUE; 
+		}
+
+		String outFileName = "m";
+		FileWriter fw = new FileWriter(outFileName);
+		PrintWriter pw = new PrintWriter(fw);
+		
+		// count is the number of file pointers that are reading the file
+		int count = n;
+		while(count > 0){
+			int min = numbers[0];
+			int minFile = 0;
+
+			for(int j = 0; j < n; j++){
+				if(min > numbers[j]){
+					min = numbers[j];
+					minFile = j;
+				}					 
+			}
+			pw.println(min);
+			String no = fp[minFile].readLine();
+			if(no != null){
+				numbers[minFile] = Integer.parseInt(no);
+			}
+			else{
+				numbers[minFile] = Integer.MAX_VALUE;
+				count--;
+			}		
+		}
+
+		for(int i = 0; i < n; i++){
+			fp[i].close();
+		}		
+		pw.close();
+		fw.close();
+
+		}
+		catch(IOException e){
+			System.out.println("Something wrong with Input");
+			e.printStackTrace();
+		}
+
+		// Stop time
+		long stopTime = System.currentTimeMillis();
+		long elapsedTime = stopTime - startTime;
+
+		return(new Time(filename, elapsedTime));
 	}
+
 
 	@Override
 	public boolean ping(){
