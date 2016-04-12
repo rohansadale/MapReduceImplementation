9232c546975e7a7cc26f79a631f3fca5ab66231c
Rohan Sadale <sadal005@trombone.cs.umn.edu>
2016-04-11 22:07:47 -0500

    Updating JobTime changes in computeNode.thrift

--- computeNode.thrift
+++ computeNode.thrift
@@ -1,8 +1,8 @@
-include "time.thrift"
+include "JobTime.thrift"
 
 service Compute
 {
-	time.Time doSort(1:string filename,2:i32 offset,3:i32 toSort),
-	time.Time doMerge(1:list<string> files),
+	JobTime.JobTime doSort(1:string filename,2:i32 offset,3:i32 toSort),
+	JobTime.JobTime doMerge(1:list<string> files),
 	bool ping()
 }
