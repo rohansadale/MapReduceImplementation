c3bef0b4f66d833e3bfadb0b18a4bb3faa6030a1
Rohan Sadale <sadal005@umn.edu>
2016-04-17 00:43:14 -0500

    Something related to Proactiveness

--- ComputeServiceHandler.java
+++ ComputeServiceHandler.java
@@ -10,28 +10,42 @@ import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TProtocol;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class ComputeServiceHandler implements ComputeService.Iface{
 
 	private String INPUT_DIRECTORY_KEY = "";
 	private String INTERMEDIATE_DIRECTORY_KEY = "";
 	private String OUTPUT_DIRECTORY_KEY = "";
+	private static String CURRENT_NODE_IP = "";
 	private float failProbability = (float)0.0;	
-    Random rnd                          = new Random();
+	Random rnd = new Random();
+	Map<String, Boolean> sortState;
+	Map<String, Boolean> mergeState;
+	Map<String, String> sortFileMap;
+	Map<String, String> mergeFileMap;
 
 	public ComputeServiceHandler(String INPUT_DIRECTORY_KEY, String INTERMEDIATE_DIRECTORY_KEY, String OUTPUT_DIRECTORY_KEY,float failProbability){
 		this.INPUT_DIRECTORY_KEY = INPUT_DIRECTORY_KEY;
 		this.INTERMEDIATE_DIRECTORY_KEY = INTERMEDIATE_DIRECTORY_KEY;	
 		this.OUTPUT_DIRECTORY_KEY = OUTPUT_DIRECTORY_KEY;	
 		this.failProbability = failProbability;
+
+		try{
+			CURRENT_NODE_IP = InetAddress.getLocalHost().getHostName();
+		}catch(Exception e){
+			System.out.println("Unable to get hostname ... ");
+		}		
 	}
+
 
 
 	// Sort Task
 	@Override
-	public JobTime doSort(String fileName, int offset, int count)
+	public JobTime doSort(String jobId, int taskId, int replId, String fileName, int offset, int count)
 	{
-		float curProbability				= rnd.nextFloat();
+		float curProbability = rnd.nextFloat();
 		if(curProbability  < this.failProbability)
 		{
 			System.out.println("Failing this Task as random number generated is less than fail probability ...");
@@ -47,7 +61,12 @@ public class ComputeServiceHandler implements ComputeService.Iface{
                }
                catch(InterruptedException ex){}
 		*/
-		String resultFileName	 = "";
+		String resultFileName  = CURRENT_NODE_IP + "_" + fileName + "_" + offset;
+		String sortStatusKey = jobId + String.valueOf(taskId) + String.valueOf(replId); 
+		sortState.put(sortStatusKey, true);
+		sortFileMap.put(sortStatusKey, resultFileName);
+
+		do{
 		try{
 			// Reading File
 			RandomAccessFile file = new RandomAccessFile(INPUT_DIRECTORY_KEY + fileName, "r");
@@ -99,27 +118,43 @@ public class ComputeServiceHandler implements ComputeService.Iface{
 				j++;
 				
 			int [] numbers = new int[temp_numbers.length-j];
-			for(int i = j; i < temp_numbers.length; i++)
+			for(int i = j; i < temp_numbers.length; i++){
 				numbers[i-j] = Integer.parseInt(temp_numbers[i]);
+				if(!sortState.get(sortStatusKey))
+					break;
+			}
+			
+			if(!sortState.get(sortStatusKey))
+				break;
+
 			Arrays.sort(numbers);
 
-			resultFileName	= INTERMEDIATE_DIRECTORY_KEY + fileName + "_" + offset;
-			FileWriter fw = new FileWriter(resultFileName);
+//			resultFileName	= INTERMEDIATE_DIRECTORY_KEY + CURRENT_NODE_IP + "_" + fileName + "_" + offset;
+			FileWriter fw = new FileWriter(INTERMEDIATE_DIRECTORY_KEY + resultFileName);
 			
 			for(int i = 0; i < numbers.length; i++){
 				fw.write(numbers[i] + " ");
+                                if(!sortState.get(sortStatusKey))
+                                        break;				
 			}			
 			fw.close();
+//			sortState[sortStatusKey] = false;
 		}
 		catch(Exception e){
+//			sortState[sortStatusKey] = false;
 			System.out.println("Error in writing to file");
 		}
+		}while(false);
 
 		long stopTime = System.currentTimeMillis();
 		long elapsedTime = stopTime - startTime;
 
+
+		if(!sortState.get(sortStatusKey))
+                return(new JobTime("", elapsedTime));			
+
 		System.out.println("Chunk Sorted for " + fileName + ", Offset - " + offset + " and Time taken =" + elapsedTime);
-		return(new JobTime(fileName+"_"+offset, elapsedTime));		
+		return(new JobTime(resultFileName, elapsedTime));		
 	} 
 	
 	@Override
@@ -130,9 +165,9 @@ public class ComputeServiceHandler implements ComputeService.Iface{
 
 	// Merge Task
 	@Override
-	public JobTime doMerge(List<String> files){
-	
-        float curProbability                = rnd.nextFloat();
+	public JobTime doMerge(String jobId, int taskId, int replId, List<String> files){
+
+	        float curProbability = rnd.nextFloat();
 		if(curProbability  < this.failProbability)	
 		{
 			System.out.println("Failing this Task as random number generated is less than fail probability ...");
@@ -143,8 +178,11 @@ public class ComputeServiceHandler implements ComputeService.Iface{
 		int n = files.size();
 		int [] numbers = new int[n];
 		Scanner[] fp = new Scanner[n];
-		String outFileName = "merge_" + Util.hashFile(files);	
+		String outFileName = CURRENT_NODE_IP + "_merge_" + Util.hashFile(files);	
 		String absolutePath = System.getProperty("user.dir");
+                String mergeStatusKey = jobId + String.valueOf(taskId) + String.valueOf(replId);
+                mergeState.put(mergeStatusKey, true);
+		mergeFileMap.put(mergeStatusKey, outFileName);
 
 		/*
 		try
@@ -153,6 +191,7 @@ public class ComputeServiceHandler implements ComputeService.Iface{
                }
                catch(InterruptedException ex){}
 		*/
+		do{
 		try{		
 			for(int i = 0; i < n; i++){
 				fp[i] = new Scanner(new File(absolutePath + "/" +  INTERMEDIATE_DIRECTORY_KEY + files.get(i)));
@@ -173,6 +212,10 @@ public class ComputeServiceHandler implements ComputeService.Iface{
 			while(count > 0){
 				int min = numbers[0];
 				int minFile = 0;
+				
+				// Check for proactiveness
+				if(!mergeState.get(mergeStatusKey))
+					break;
 	
 				for(int j = 0; j < n; j++){
 					if(min > numbers[j]){
@@ -180,7 +223,7 @@ public class ComputeServiceHandler implements ComputeService.Iface{
 						minFile = j;
 					}					 
 				}
-				if(min!=Integer.MAX_VALUE) 
+				if(min != Integer.MAX_VALUE) 
 					pw.print(String.valueOf(min) + " ");
 				
 				if(fp[minFile].hasNextInt()){
@@ -191,7 +234,11 @@ public class ComputeServiceHandler implements ComputeService.Iface{
 					count--;
 				}		
 			}
-	
+			
+			// Check for proactiveness
+			if(!mergeState.get(mergeStatusKey))
+                        	break;
+
 			for(int i = 0; i < n; i++){
 				fp[i].close();
 			}		
@@ -202,18 +249,57 @@ public class ComputeServiceHandler implements ComputeService.Iface{
 			for(int i = 0; i < files.size(); i++){
 				File f = new File(absolutePath + "/" + INTERMEDIATE_DIRECTORY_KEY + files.get(i));
 				f.delete();
-			}
+
+				// Check for proactiveness
+                                if(!mergeState.get(mergeStatusKey))
+                                        break;
+			}			
 		}
 		catch(IOException e){
 			System.out.println("Something wrong with Input");
 			e.printStackTrace();
 		}
+		}while(false);
 
 		// Stop time
 		long stopTime = System.currentTimeMillis();
 		long elapsedTime = stopTime - startTime;
+
+		if(!mergeState.get(mergeStatusKey))
+			return(new JobTime("", elapsedTime));
+
 		System.out.println("Merge Completed! Time taken = " + elapsedTime);
 		return(new JobTime(outFileName, elapsedTime));
+	}
+
+
+	public boolean updateStatus(String jobId, int taskId, int replId, int type, boolean shouldStop){
+		String key = jobId + String.valueOf(taskId) + String.valueOf(replId);
+		String fileName = "";
+		if(type == 0){
+			sortState.put(key, false);
+			fileName = sortFileMap.get(key);
+		}
+		else{
+			mergeState.put(key, false);
+			fileName = mergeFileMap.get(key);
+		}
+		
+                String absolutePath = System.getProperty("user.dir");
+		File file = new File(absolutePath + INTERMEDIATE_DIRECTORY_KEY + fileName);
+		file.delete();
+		return true;
+	}
+
+	public void cleanJob(String key){
+		if(mergeState.containsKey(key))
+			mergeState.remove(key);
+		if(mergeFileMap.containsKey(key))
+			mergeFileMap.remove(key);
+		if(sortState.containsKey(key))
+			sortState.remove(key);
+		if(sortFileMap.containsKey(key))
+			sortFileMap.remove(key);			
 	}
 
 
